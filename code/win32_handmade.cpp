d199f958fe4d4af39ca9d3922db3f6273c616141
Daniel Björnlund <danielbjornlund@daniels-mbp.mynet>
2021-12-02 18:53:03 +0100

    added win32_handmade.cpp

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+INT WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	PSTR lpCmdLine,
+ 	INT nCmdShow) 
+{
+	MessageBox(0, "This is handmade hero", "Handmade Hero", HB_OK | MB_ICONERROR);
+	retun 0;
+}
+
+
