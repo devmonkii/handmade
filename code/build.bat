73d9a28e25e86b33f36fb60866d1208b20a0a096
Daniel Björnlund <danielbjornlund@daniels-mbp.mynet>
2021-12-03 17:36:54 +0100

    Added Gdi32.lib to build.bat and added handler for WM_PAINT message

--- code/build.bat
+++ code/build.bat
@@ -1,4 +1,4 @@
 @echo off
 mdkir ..\..\build
 pushd ..\..\build
-cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib
+cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib Gdi32.lib
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,22 +1,89 @@
 #include <windows.h>
 
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LParam) {
-	
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
+	LRESULT Result = 0;
+
+	switch(Message) {
+		case WN_SIZE: {
+			OutputDebugStringA("WN_SIZE\n")
+		}
+		break;
+		case WN_DESTROY: {
+			OutputDebugStringA("WN_DESTROY\n")
+		}
+		break;
+		case WN_CLOSE: {
+			OutputDebugStringA("WN_CLOSE\n")
+
+		}
+		break;
+		case WM_ACTIVATEAPP: {
+			OutputDebugStringA("WM_ACTIVATEAPP\n")
+
+		}
+		break;
+		case WM_PAINT: {
+			
+			PAINTSTRUCT Paint;			
+			HDC DeviceContext = BeginPaint(Window, &Paint)
+
+			INT X       = Paint.rcPaint.left;
+			INT Y       = Paint.rcPaint.right;
+			LONG Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			LONG Width  =  Paint.rcPaint.right - Paint.rcPaint.left;
+
+
+			PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS)
+
+
+			EndPaint(Window, &Paint);
+		}
+		default: {
+			OutputDebugStringA("DEFAULT\n")
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		}
+		break;
+	}
+
+	retun Result;
 }
 
-INT WINAPI WinMain(
-	HINSTANCE Instance,
-	HINSTANCE PrevInstance,
-	PSTR CommandLine,
- 	INT ShowCode) {
+INT WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT ShowCode) {
 	
-	WNDCLASS WindowClass = {};
 
+	WNDCLASS WindowClass      = {};
 	WindowClass.style         = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-	WindowClass.lpfnWndProc   = *MainWindowCallback;
+	WindowClass.lpfnWndProc   = &MainWindowCallback;
 	WindowClass.hInstance     = Instance;
 	WindowClass.lpszClassName = "HandmadeHeroWindowClass"
 
+	RegisterClass(&WindowClass)
+	
+	HWND WindowHandle = CreateWindowEx(
+		0,
+		WindowClass.lpszClassName,
+		"Handmade Hero",
+		WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		Instance,
+		0);
+
+	if(WindowHandle) {
+		for(;;) {
+			MSG Message;
+			BOOL MessageResult = GetMessage(&Message, 0, 0, 0,0 )
+			if(MessageResult > 0) {
+
+			} else {
+				break;
+			}
+		}
+	}
 	retun 0;
 }
